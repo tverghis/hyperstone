aac01086f0e8ba1e3ad283e03b18503fb406f61c
Tarun Verghis <tarun.verghis@gmail.com>
2019-07-16 17:38:38 -0700

    Add ability to stop the parser

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ mod protos;
 pub struct Hyperstone {
     current_tick: u32,
     stop_at_tick: Option<u32>,
+    is_stopping: bool,
 }
 
 impl Hyperstone {
@@ -15,13 +16,22 @@ impl Hyperstone {
         }
     }
 
+    /// Starts parsing the replay buffer from the beginning.
     pub fn begin_parse(&self, input: &[u8]) {
-        let (input, _) = combinators::take_source2_signature(input).unwrap();
+        let input = match combinators::take_source2_signature(input) {
+            Ok((inp, _)) => inp,
+            _ => panic!("Couldn't verify Source 2 signature in the provided file."),
+        };
+
         let (input, _) = combinators::take_replay_size_info(input).unwrap();
 
         let mut input = input;
 
         loop {
+            if self.is_stopping {
+                break;
+            }
+
             if let Some(tick) = self.stop_at_tick {
                 if self.current_tick >= tick {
                     break;
@@ -30,12 +40,25 @@ impl Hyperstone {
 
             input = match combinators::take_outer_message(input) {
                 Ok((remainder, _)) => remainder,
-                _ => {
-                    println!("Unknown outer message.");
-                    std::process::exit(1);
-                }
+                _ => panic!("Unknown outer message."),
             };
         }
+    }
+
+    /// Signals the parser to stop parsing any further.
+    pub fn stop_parse(&mut self) {
+        self.is_stopping = true;
+    }
+
+    /// Signals the parser to stop parsing at a specific game tick.
+    /// If `stop_tick` has already been passed, this will do nothing.
+    pub fn stop_parse_at_tick(&mut self, stop_tick: u32) {
+        self.stop_at_tick = Some(stop_tick);
+    }
+
+    /// Returns the last game tick that the parser processed.
+    pub fn current_tick(&self) -> u32 {
+        self.current_tick
     }
 }
 
