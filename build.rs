4380fdc58264956e4360e605cdb9a6ca5350c228
Tarun Verghis <tarun.verghis@gmail.com>
2019-07-18 21:43:33 -0700

    Smarter build.rs
    build.rs now considers non-empty destination
    directory before generating files. It also
    generates the necessary mod.rs file.

--- build.rs
+++ build.rs
@@ -1,8 +1,29 @@
 use protoc_rust::Customize;
-use std::fs;
+use std::fs::{self, File};
+use std::io::Write;
 use std::process;
 
 fn main() {
+    // First check if the src/protos directory is empty or not...
+    match fs::read_dir("./src/protos") {
+        Ok(src_dir_contents) => {
+            if src_dir_contents.count() > 0 {
+                println!("src/protos is non-empty, not building protobufs.");
+                process::exit(0);
+            }
+        }
+        _ => {
+            println!("No src/protos directory found.");
+            match fs::create_dir("./src/protos") {
+                Ok(_) => println!("src/protos directory created."),
+                Err(e) => {
+                    eprintln!("Failed to create src/protos: {}", e);
+                    process::exit(1);
+                }
+            }
+        }
+    };
+
     let dir_contents = fs::read_dir("./protos").unwrap();
     let mut file_names = Vec::new();
 
@@ -31,4 +52,20 @@ fn main() {
             process::exit(1);
         }
     }
+
+    match gen_mod_rs() {
+        Ok(_) => println!("Generated src/protos/mod.rs"),
+        Err(e) => {
+            eprintln!("Failed to generate src/protos/mod.rs: {}", e);
+            process::exit(1);
+        }
+    }
+}
+
+fn gen_mod_rs() -> std::io::Result<()> {
+    let mut modrs = File::create("./src/protos/mod.rs")?;
+
+    modrs.write_all(b"pub mod demo;\n")?;
+
+    Ok(())
 }
